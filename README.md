fb9e2c5baa7f4d308727190fe563452e63b12ebf
gloryyin <gloryyin@tencent.com>
2021-09-19 16:02:31 +0800

    feat: 增加几个网页 demo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # front-end-demo
 web课程课前预习材料的几个 demo
+## HTML
+一个简单的 HTML 例子
+## CSS
+一个带导航菜单的网页
+## JavaScript
+一个支持搜索功能的搜索框
---
+++ demos/javascript/index.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+  const myInput = document.querySelector("#myInput");
+  myInput.addEventListener("keyup", function () {
+    // 声明变量
+    let input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+
+    // 循环所有列表，查找匹配项
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  });
+};
